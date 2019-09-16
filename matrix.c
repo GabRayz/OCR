c7c58531a11fb83f58cd7f91db434b76930e0d8c
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2019-09-16 20:14:59 +0200

    Matrix: Add doc and error logs

--- matrix.c
+++ matrix.c
@@ -3,19 +3,28 @@
 #include <math.h>
 #include "matrix.h"
 
+/* 
+    Matrices declaration and operations
+    struct matrix:
+        width: The width of the matrix
+        height: The height of the matrix
+        content: The content of the matrix, represented by an array of arrays of floats
+*/
+
 Matrix *m_init(int height, int width)
 {
+    // Allocate the right size
     Matrix *m = malloc(sizeof(Matrix));
     m->height = height;
     m->width = width;
-    m->content = malloc(sizeof(double *) * height);
+    m->content = malloc(sizeof(double *) * height); // Size of an array of float, height times
 
     for (int y = 0; y < height; y++)
     {
-        m->content[y] = malloc(sizeof(double) * width);
+        m->content[y] = malloc(sizeof(double) * width); // Allocate the size of float, width times
         for (int x = 0; x < width; x++)
         {
-            m->content[y][x] = 0;
+            m->content[y][x] = 0; // Set all values to 0
         }
     }
 
@@ -24,7 +33,7 @@ Matrix *m_init(int height, int width)
 
 Matrix *m_init_values(int height, int width, double *content)
 {
-    Matrix *m = m_init(height, width);
+    Matrix *m = m_init(height, width); // Create an empty matrix
 
     for (int y = 0; y < height; y++)
     {
@@ -39,13 +48,14 @@ Matrix *m_init_values(int height, int width, double *content)
 
 void m_delete(Matrix *m)
 {
+    // Delete the matrix to free memory space
     for (int y = 0; y < m->height; y++)
     {
-        free(m->content[y]);
+        free(m->content[y]); // Free all float arrays
     }
 
-    free(m->content);
-    free(m);
+    free(m->content); // Free the array of array
+    free(m); // Free the struct
 }
 
 void m_print(Matrix *m)
@@ -64,7 +74,7 @@ Matrix *m_add(Matrix *a, Matrix *b)
 {
     if (a->width != b->width || a->height != b->height)
     {
-        printf("PAS BIEN SALO\n");
+        printf("Matrix addiction: Sizes are not valid\n");
         exit(1);
     }
 
@@ -85,7 +95,7 @@ Matrix *m_sub(Matrix *a, Matrix *b)
 {
     if (a->width != b->width || a->height != b->height)
     {
-        printf("PAS BIEN SALO\n");
+        printf("Matrix substraction: Sizes are not valid\n");
         exit(1);
     }
 
@@ -106,7 +116,7 @@ Matrix *m_mult(Matrix *a, Matrix *b)
 {
     if (a->width != b->height)
     {
-        printf("PAS BIEN SALO\n");
+        printf("Matrix product: Sizes are not valid\n");
         exit(1);
     }
 
@@ -165,6 +175,10 @@ Matrix *m_div(Matrix *m, double value)
 {
     return m_mult_num(m, 1 / value);
 }
+
+/* 
+    Useful matrices operations in backpropagation computations
+*/
 
 Matrix *m_hadamard(Matrix *a, Matrix *b)
 {
@@ -228,7 +242,7 @@ Matrix *m_sigmoid_prime(Matrix *m)
         for (int x = 0; x < m->width; x++)
         {
             double sig = sigmoid(res->content[y][x]);
-            res->content[y][x] = sig * (1 - sig);
+            res->content[y][x] = sig * (1 - sig); // Just compute the derivative of sigmoid
         }
     }
     return res;
@@ -236,6 +250,8 @@ Matrix *m_sigmoid_prime(Matrix *m)
 
 Matrix *m_tanspose(Matrix *m)
 {
+    // Transpose rows and colomns
+
     Matrix *res = m_init(m->width, m->height);
 
     for (int y = 0; y < m->height; y++)
@@ -250,6 +266,7 @@ Matrix *m_tanspose(Matrix *m)
 
 float m_sum(Matrix *m)
 {
+    // Make the sum of all the matrix values
     float res = 0;
     for (int y = 0; y < m->height; y++)
     {
@@ -264,7 +281,20 @@ float m_sum(Matrix *m)
 
 Matrix *m_average(Matrix **list, int count)
 {
-    // TODO: Vérifier les dimensions des matrices
+    if (count < 1) {
+        printf("Matrix average: List must not be empty\n");
+        exit(1);
+    }
+    int height = list[0]->height;
+    int width = list[0]->width;
+    for (int i = 0; i < count; i++)
+    {
+        if(list[i]->height != height || list[i]->width != width) {
+            printf("Matrix average: All matrices of the list must have the same size");
+            exit(2);
+        }
+    }
+    
     Matrix *res = m_init(list[0]->height, list[0]->width);
 
     for (int y = 0; y < list[0]->height; y++)
