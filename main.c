5cb04cb999b21c48ccab8c58c6b3927e1018b42d
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2019-09-20 18:35:52 +0200

    read_dataset() to get bmp files names

--- main.c
+++ main.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
+#include <string.h>
+#include <dirent.h>
 #include "matrix.h"
 #include "neuralnetwork.h"
 #include <ImageMagick-7/MagickWand/MagickWand.h>
+
+void read_dataset(char *files[], int dataCount)
+{
+    /* 
+    Read the dataset/training directory to get images names
+     */
+    DIR *dataset;
+    struct dirent *dir;
+    dataset = opendir("./dataset/training/");
+    if (dataset)
+    {
+        // For each file up to dataCount
+        int i = 0;
+        while ((dir = readdir(dataset)) != NULL && i < dataCount)
+        {
+            // Store the file name in the array
+            files[i] = dir->d_name;
+            i++;
+        }
+        closedir(dataset);
+    }
+}
 
 int main(/* int argc, char **argv */)
 {
@@ -11,7 +35,17 @@ int main(/* int argc, char **argv */)
     nn_setupRandom(nn);
     nn_delete(nn);
 
-    MagickWand *mw = NewMagickWand();
+    // MagickWand *mw = NewMagickWand();
+
+    //char files[2][14];
+    // char files[2][14] = *read_dataset(5);
+    char* files[5];
+    read_dataset(files, 5);
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%s\n", files[i]);
+    }
     
+
     return 0;
 }
