b9761abe6336741b6d1aaa6ed462a8b0dbc8f29f
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2019-09-21 15:04:43 +0200

    wtf

--- main.c
+++ main.c
@@ -23,7 +23,30 @@ void read_dataset(char *files[], int dataCount)
             files[i] = dir->d_name;
             i++;
         }
-        closedir(dataset);
+        closedir(dataset); 
+    }
+}
+
+void dataset_to_pixels(char *files[], int dataCount) {
+    for (int i = 0; i < dataCount; i++)
+    {
+        //printf("%s\n", files[i]);
+        
+        char file[100];
+        strcpy(file, "dataset/training/");
+        strcat(file, files[i]);
+        printf("%s\n", file);
+
+        
+        MagickWand *wand = NewMagickWand();
+        // MagickReadImage(wand, file);
+        // if (MagickReadImage(wand, file) == MagickTrue) {
+        //     printf("File open successfuly\n");
+        // }else {
+        //     printf("Failed to open file: %s\n", file);
+        // }
+        // ClearMagickWand(wand);
+        DestroyMagickWand(wand);
     }
 }
 
@@ -36,15 +59,29 @@ int main(/* int argc, char **argv */)
     nn_delete(nn);
 
     // MagickWand *mw = NewMagickWand();
+    // MagickReadImage(mw, "dataset/training/a_AmericanTypewriter_-1-1.bmp");
+    // printf("%d\n", (int)MagickGetImageHeight(mw));
 
-    //char files[2][14];
-    // char files[2][14] = *read_dataset(5);
     char* files[5];
     read_dataset(files, 5);
-    for (int i = 0; i < 5; i++)
+
+    FILE *input;
+    int get_char;
+    input = fopen("dataset/training/a_AmericanTypewriter_-1-1.bmp", "r");
+    // printf("%c\n", fgetc(input));
+    printf("Hey\n");
+    while((get_char=fgetc(input))!= EOF)
     {
-        printf("%s\n", files[i]);
+        printf("%d", get_char);
     }
+    fclose(input);
+
+    //dataset_to_pixels(files, 5);
+
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     printf("%s\n", files[i]);
+    // }
     
 
     return 0;
