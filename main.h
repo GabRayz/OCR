85d2a9e62ca0082487b05de1aff6f9adce124316
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2019-10-20 18:50:11 +0200

    Complete main.h

--- main.h
+++ main.h
@@ -4,7 +4,11 @@
 #include "neuralnetwork.h"
 #include "segmentation.h"
 
-NeuralNetwork *create_nn();
+NeuralNetwork *create_nn(char *filepath, int cycles);
 char *send_to_cerveau(Img *source, LinkedList *chars, NeuralNetwork *nn);
+void save_res(char *res, char *filepath);
+int write_dataset(int argc, char **argv);
+int learn(int argc, char **argv);
+int read_image(int argc, char **argv);
 
 #endif
