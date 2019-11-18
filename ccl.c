34f79e6e24bcd042db6e028757dd235d52b9249c
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2019-11-18 11:47:53 +0100

    CCL histogram

--- ccl.c
+++ ccl.c
@@ -2,10 +2,38 @@
 #include "image.h"
 #include "segmentation.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 void ccl_segmentation(Img *source, Block *block)
 {
     int *hist = calloc(block->width * block->height, sizeof(int));
+    LinkedList *labels = create_hist(source, block, hist);
+    print_ccl(source, block, labels);
+}
+
+void print_ccl(Img *source, Block *block, LinkedList *labels)
+{
+    Node *n = labels->start;
+    while (n)
+    {
+        Block *b = n->data;
+        // printf("%d\n", b->width);
+        for (int y = b->y; y < b->y + b->height; y++)
+        {
+            for (int x = b->x; x < b->x + b->width; x++)
+            {
+                source->pixels[(y) * source->width + x] = 0;
+            }
+        }
+        n = n->next;
+    }
+
+    img_save(source, "res/ccl.png");
+}
+
+LinkedList *create_hist(Img *source, Block *block, int *hist)
+{
+    LinkedList *labels = list_init();
     int currentLabel = 1;
     // Iterates through each pixel
     for (int y = 0; y < block->height; y++)
@@ -15,21 +43,39 @@ void ccl_segmentation(Img *source, Block *block)
             // If the pixel is black and not labeled
             if (hist[y * block->width + x] == 0 && source->pixels[(y + block->y) * source->width + x + block->x] == 0)
             {
+                // Create the block that will store the size and location
+                Block *current = block_init();
+                current->x = x;
+                current->y = y;
                 // Start propagation and increment the label
-                propagate(source, block, hist, x, y, currentLabel);
+                propagate(source, block, current, hist, x, y, currentLabel);
                 currentLabel++;
+                Node *node = node_init(current);
+                list_insert(labels, node);
             }
         }
     }
+
+    return labels;
 }
 
-void propagate(Img *source, Block *block, int *hist, int x, int y, int currentLabel)
+void propagate(Img *source, Block *block, Block *current, int *hist, int x, int y, int currentLabel)
 {
+    // Check if the pixel exists and is not labeled
     if (x < 0 || x >= block->width || y < 0 || y >= block->height || hist[y * block->width + x] != 0)
         return;
     if (source->pixels[(y + block->y) * source->width + x + block->x] == 1)
         return;
     hist[y * block->width + x] = currentLabel;
+    // Update block's coordinates
+    if (x < current->x)
+        current->x = x;
+    if (y < current->y)
+        current->y = y;
+    if (x - current->x > current->width)
+        current->width = x - current->x + 2;
+    if (y - current->y > current->height)
+        current->height = y - current->y + 2;
     for (int i = -1; i < 2; i++)
     {
         for (int j = -1; j < 2; j++)
@@ -37,7 +83,7 @@ void propagate(Img *source, Block *block, int *hist, int x, int y, int currentLa
             if (j == 0 && i == 0)
                 continue;
 
-            propagate(source, block, hist, x + j, y + i, currentLabel);
+            propagate(source, block, current, hist, x + j, y + i, currentLabel);
         }
     }
 }
--- ccl.h
+++ ccl.h
@@ -5,6 +5,8 @@
 #include "segmentation.h"
 
 void ccl_segmentation(Img *source, Block *block);
-void propagate(Img *source, Block *block, int *hist, int x, int y, int currentLabel);
+void print_ccl(Img *source, Block *block, LinkedList *labels);
+LinkedList *create_hist(Img *source, Block *block, int *hist);
+void propagate(Img *source, Block *block, Block *current, int *hist, int x, int y, int currentLabel);
 
 #endif
